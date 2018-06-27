b69e0452104e45a6fc6ec13b81820bf479b11548
Oliver <oliveroenang@gmail.com>
2018-06-27 21:08:24 +0200

    heroku issue

--- helpers.js
+++ helpers.js
@@ -1,5 +1,2 @@
-// one helper so I can use this in my templates using h.moment.
-exports.moment = require('moment');
-
 // console logging from template
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
